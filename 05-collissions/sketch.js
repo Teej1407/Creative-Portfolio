43ade213975e8403305a8416a893bbd6f5779acc
Tyler <tylerjwilliams222@gmail.com>
2024-12-03 02:06:08 +0000

    final

--- 05-collissions/sketch.js
+++ 05-collissions/sketch.js
@@ -4,28 +4,35 @@ let y;
 let myFill = 255;
 
 function setup() {
-  createCanvas(500, 500); // canvas width/height
-  x=width/2;
-  y=height/2;
+  createCanvas(400, 400); // canvas width/height
+  x=width = 240;
+  y=height= 360;
+
 }
 
 function draw(){
   background(100); // Make background grey
 
   fill(myFill);
-  ellipse(x, y, size); // x, y, size
-  
+  text('Find the correct area and make shapes red!!!',50,50)
+  ellipse(25, 25, size); // x, y, size
+  ellipse(375, 25, size);
+  rect(100, 200,50);
+  triangle(15, 300, 58, 260, 100, 300);
+  triangle(300, 150, 58, 260, 100, 300);
+  ellipse(300, 289, 78);
+  ellipse(300, 289, 20);
+  ellipse(300, 289, 10);
+
   fill(150)
   ellipse(mouseX, mouseY, size); // x, y, size
   
   let distance = dist(x, y, mouseX, mouseY); 
   if(distance <= size/2){
-    myFill = [255, 0, 0];
+    myFill = [255, 45, 0];
   }else{
     myFill = 255;
   }
 
-  fill(0)
-  textSize(15);
-  text(int(distance), x-15, y-10, 30);
+ 
 }
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-Hello
-Hello
-hello
-
-<>
+Having little to no experiace with coding i struggled to bigin with but eventually started to get my head around the concept
+in the end im proud of what ive learned and achived and will continue to explore what else i can do.
